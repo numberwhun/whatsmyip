631fb6acbd43a8071ba06e113be1f38c50ba810a
Jefferson Kirkland <numberwhun@gmail.com>
2013-09-18 17:32:00 -0400

    Updated script to look for active interfaces and only report the interfaces with an ip. This revision for Mac.

--- whatsmyip.sh
+++ whatsmyip.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
 
-###########################################
+####################################################################
 #
 # script_name = whatsmyip.sh
 # author_name = Jefferson Kirkland
 #     Company = ParsedContent Solutions
 #
-###########################################
+####################################################################
 #
 # When this script is executed, it will try to determine if it is
 # beig run on either Solaris, Linux or Mac OSx.  It then determines
 # the IP and prints it out.
 #
-###########################################
+####################################################################
 
+### Functions ###
+get_macnics() {
+	ifconfig  | grep UP | grep en | cut -d: -f1
+}
+
+get_macip() {
+	for i in "${Interfaces[@]}"
+	do
+		if [[ $(ifconfig $i | grep inet) ]]
+		then
+			Upinterfaces=("${Upinterfaces[@]}" $(ifconfig $i | grep inet | grep -v "inet6" | cut -d' ' -f2));
+		fi
+	done
+	for t in "${Upinterfaces[@]}"
+    do  
+		echo $t
+    done
+}
+
+### Array Declarations
+declare -a Interfaces=()
+declare -a Upinterfaces=()
+
+### Variable Declarations
 system=$(uname)
 
 if [ $system == "Darwin" ]
 then
-	/sbin/ifconfig en1 | grep "inet" | grep -v "inet6" | cut -d' ' -f2
+	Interfaces=("${Interfaces[@]}" $(get_macnics));
+	get_macip
+	#for i in "${Interfaces[@]}"
+	#do
+	#	#echo $i
+	#	if [[ $(ifconfig $i | grep inet) ]]
+	#	then
+	#		Upinterfaces=("${Upinterfaces[@]}" $(ifconfig $i | grep inet | grep -v "inet6" | cut -d' ' -f2));
+	#	fi
+	#done
+	#for t in "${Upinterfaces[@]}"
+    #do  
+	#	echo $t
+    #done
 elif [ $system == "Linux" ]
 then
 	/sbin/ifconfig eth0 | grep “inet addr” | cut -d: -f2 | cut -d’ ‘ -f1
