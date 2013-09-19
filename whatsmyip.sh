5ccc0291d8069122fed0ccef4c998cb9be786f90
Jefferson Kirkland <numberwhun@gmail.com>
2013-09-18 23:37:21 -0400

    Updates to linux and solaris bits

--- whatsmyip.sh
+++ whatsmyip.sh
@@ -11,12 +11,20 @@
 # When this script is executed, it will try to determine if it is
 # beig run on either Solaris, Linux or Mac OSx.  It then determines
 # the IP and prints it out.
-#
+# 
 ####################################################################
 
 ### Functions ###
 get_macnics() {
 	ifconfig  | grep UP | grep en | cut -d: -f1
+}
+
+get_linuxnics() {
+	ifconfig | grep inet | grep -v inet6 | grep -v "127.0" | cut -d: -f2 | cut -d' ' -f1
+}
+
+get_solarisnics() {
+	ifconfig | grep inet | grep -v "127.0" | cut -d' ' -f2
 }
 
 get_macip() {
@@ -33,6 +41,7 @@ get_macip() {
     done
 }
 
+
 ### Array Declarations
 declare -a Interfaces=()
 declare -a Upinterfaces=()
@@ -44,21 +53,9 @@ if [ $system == "Darwin" ]
 then
 	Interfaces=("${Interfaces[@]}" $(get_macnics));
 	get_macip
-	#for i in "${Interfaces[@]}"
-	#do
-	#	#echo $i
-	#	if [[ $(ifconfig $i | grep inet) ]]
-	#	then
-	#		Upinterfaces=("${Upinterfaces[@]}" $(ifconfig $i | grep inet | grep -v "inet6" | cut -d' ' -f2));
-	#	fi
-	#done
-	#for t in "${Upinterfaces[@]}"
-    #do  
-	#	echo $t
-    #done
 elif [ $system == "Linux" ]
 then
-	/sbin/ifconfig eth0 | grep “inet addr” | cut -d: -f2 | cut -d’ ‘ -f1
+	get_linuxnics
 elif [ $system == "SunOS" ]
 then
 	/sbin/ifconfig bge0 | grep "inet" | cut -d' ' -f2
